7ea39e37e1924e145c05eaaf1def860b261deaec
premnath <premdevapp@gmail.com>
2022-02-26 18:31:59 +0530

    changes

---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router"
+import styles from "../styles/sidebar.module.css";
+
+const Sidebar = () =>{
+  return <div className={styles.wrapper}>sidebar</div>
+}
+
+export default Sidebar;
